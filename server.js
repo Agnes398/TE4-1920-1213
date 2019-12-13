49e23f26e8ab42ddd7c6aa05660ebbdb6c13d99d
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-12-13 17:05:05 +0100

    it's alive test successful locally

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const BodyParser = require('body-parser');
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send("I am also alive");
+}); 
+
+app.listen(4242, () => {
+    console.log("I am alive");
+});
