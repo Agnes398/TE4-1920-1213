d29cd2a8dfa33bd0987ab7ee4f33f1295cf43340
Agnes Löjdkvist <agnes.lojdkvist@edu.huddinge.se>
2019-12-13 17:25:22 +0100

    successful access to db and recieve + send info, locally

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const BodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const uri = "mongodb+srv://Agnes:Britta398@acme-oij5h.mongodb.net/test?retryWrites=true&w=majority";
 
 const port = process.env.PORT;
 
+var collection;
 const app = express();
+const client = new MongoClient(uri, {useNewUrlParser: true}, {useUnifiedTopology: true});
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended: true}));
@@ -12,6 +17,15 @@ app.get('/', (req, res) => {
     res.send("I am also stayin alive");
 }); 
 
-app.listen(port, () => {
-    console.log("I am alive");
+app.get('/notes', (req, res) => {
+    collection.find({}).toArray((err, result) => {
+        res.send(result);
+    })
+});
+
+app.listen(4242, () => {
+    client.connect(err => {
+        collection = client.db("Acme").collection("Notes");
+        console.log('Connect to db');
+    });
 });
